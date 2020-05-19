05de086cbb41a0f2e939c7747ee21771c3f3b841
Wesley <a13207123727@gmail.com>
2020-05-20 00:23:31 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,9 @@ cd /root/vuepress/wiki
 git pull
 npm run build
 
-# 挂载oss
-# ossfs iclass-api-wsg-sz /oss/iclass-api -ourl=oss-cn-shenzhen-internal.aliyuncs.com
-# 准备发布
-rm -rf /oss/wiki/*
-cp -R /root/vuepress/wiki/docs/.vuepress/dist/* /oss/wiki
-
-# rm -rf /root/nginx/html/wiki/*
-# cp -R /root/vuepress/wiki/docs/.vuepress/dist/* /root/nginx/html/wiki
+# 发布
+rm -rf /root/nginx/html/wiki/*
+cp -R /root/vuepress/wiki/docs/.vuepress/dist/* /root/nginx/html/wiki
 
 # 提交到git
 git add .
