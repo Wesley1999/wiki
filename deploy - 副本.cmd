4b869074512bb91d52f2c8d53beecf3109e14616
unknown <a13207123727@gmail.com>
2020-05-22 20:52:44 +0800

    Windows commit on 2020/05/22 周五 20:52:44.40

--- deploy - 副本.cmd
+++ deploy - 副本.cmd
@@ -3,5 +3,5 @@ git add .
 git commit -m "Windows commit on %date% %time%"
 git pull
 git push
-curl http://120.76.62.52:3000/wiki/deploy
+ssh root@120.76.62.52
 exit
---
+++ docs/test.md
@@ -0,0 +1,3 @@
+# Just A Test
+
+test.
