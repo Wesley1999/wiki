c5e6ff44dfc6c4797bcae207b5a3cb71d0ab3589
unknown <a13207123727@gmail.com>
2020-05-22 21:17:14 +0800

    Windows commit on 2020/05/22 周五 21:17:14.06

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,9 @@
+mshta vbscript:msgbox("点击确定后开始发布",6,"提示")(window.close)
 cd D:\code\wiki
 git add .
 git commit -m "Windows commit on %date% %time%"
 git pull
 git push
-echo '发布请求已提交到服务器，发布成功后，本窗口将自动关闭'
 curl http://120.76.62.52:3000/wiki/deploy
+mshta vbscript:msgbox("发布成功",6,"提示")(window.close)
 exit
